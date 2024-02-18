020e252a557e4f9305e865a835e819493da2a654
Patrick Soisson <psoysauce@gmail.com>
2024-02-17 21:04:05 -0600

    Created Credit Card Component

---
+++ credit-card/src/components/Credit-Card/Credit-Card.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { CreditCardWrapper } from './Credit-Card.styled';
+
+const CreditCard = () => (
+ <CreditCardWrapper>
+    CreditCard Component
+ </CreditCardWrapper>
+);
+
+CreditCard.propTypes = {};
+
+CreditCard.defaultProps = {};
+
+export default CreditCard;
